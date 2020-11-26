81855685aedbc01a6e28a44b8fcc516fd5bcca48
Cyril Rohr <cyril.rohr@gmail.com>
2020-11-26 15:10:55 +0000

    Run feature specs with only one thread

--- docker/ci/entrypoint.sh
+++ docker/ci/entrypoint.sh
@@ -9,14 +9,11 @@ echo "create database app; create user app with superuser encrypted password 'p4
 
 mkdir -p /usr/local/bundle
 mkdir -p /home/$USER/openproject/frontend/node_modules
-# mkdir -p /home/$USER/openproject/public/assets/frontend
 mkdir -p /home/$USER/openproject/tmp
 
 chown $USER:$USER /usr/local/bundle
 chown $USER:$USER /home/$USER/openproject/frontend/node_modules
-# chown $USER:$USER /home/$USER/openproject/public/assets/frontend
 chown $USER:$USER /home/$USER/openproject/tmp
-# chown -R $USER:$USER /home/$USER/openproject
 
 cp docker/ci/database.yml config/
 
